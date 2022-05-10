f8949fe49d241c40265e3f0a33c76b6414a781a9
lizzochek <littled685@gmail.com>
2022-05-10 21:36:01 +0300

    Game over implemented

--- monster-slayer/app.js
+++ monster-slayer/app.js
@@ -7,6 +7,7 @@ Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -18,6 +19,20 @@ Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0) {
+        this.winner = "player";
+      }
     },
   },
   methods: {
