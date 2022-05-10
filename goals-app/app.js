43fe37e85c825a807684d2f20d43b0df32ad0268
lizzochek <littled685@gmail.com>
2022-05-10 14:19:48 +0300

    This keyword

--- goals-app/app.js
+++ goals-app/app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Learn Vue",
+      courseGoalA: "Learn Vue",
+      courseGoalB: "Master Vue",
       vueLink: "https://vuejs.org/guide/introduction.html",
     };
   },
   methods: {
     chooseGoal() {
       const randomNum = Math.random();
-      return randomNum < 0.5 ? "Learn Vue!" : "Master Vue";
+      return randomNum < 0.5 ? this.courseGoalA : this.courseGoalB;
     },
   },
 });
