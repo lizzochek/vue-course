87c09dfdb6b583de3750846936a6a31e54074221
lizzochek <littled685@gmail.com>
2022-05-10 18:08:00 +0300

    Dynamic-styling

---
+++ dynamic-styling/app.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+    boxBClasses() {
+      return { active: this.boxBSelected };
+    },
+    boxCClasses() {
+      return { active: this.boxCSelected };
+    },
+  },
+  methods: {
+    boxSelected(box) {
+      switch (box) {
+        case "A":
+          this.boxASelected = !this.boxASelected;
+          break;
+        case "B":
+          this.boxBSelected = !this.boxBSelected;
+          break;
+        case "C":
+          this.boxCSelected = !this.boxCSelected;
+          break;
+      }
+    },
+  },
+}).mount("#styling");
