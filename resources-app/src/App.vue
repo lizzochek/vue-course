ffff716375e1c0dcb38987a74c2a27f720fe7ddb
lizzochek <littled685@gmail.com>
2022-05-12 00:07:36 +0300

    Creating an appp

---
+++ resources-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You need to google!',
+          link: 'https://google/com',
+        },
+      ],
+    };
+  },
+};
+</script>
