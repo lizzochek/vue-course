89681d07e99e9237f6417c34ec0ec57024843306
lizzochek <littled685@gmail.com>
2022-05-11 14:56:52 +0300

    Components with Vue CLI

---
+++ vue-app/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsVisible">
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsVisible: true,
+      friend: {
+        id: "liza",
+        name: "Liza Dolhova",
+        phone: "0523226586",
+        email: "liza@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+  },
+};
+</script>
