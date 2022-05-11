8aeaaba51ef413a42c40d3a479761578b57aae40
lizzochek <littled685@gmail.com>
2022-05-11 22:28:44 +0300

    Slots

---
+++ more-components/src/components/CoarseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="(goal, index) in goals" :key="index">
+      <slot :item="goal"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Learn Vue", "Become a master"],
+    };
+  },
+};
+</script>
