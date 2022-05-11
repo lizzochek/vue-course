cf50c17672ed69c11f3d8db763030511f29cc227
lizzochek <littled685@gmail.com>
2022-05-11 23:39:38 +0300

    Error alert

--- more-components/src/App.vue
+++ more-components/src/App.vue
@@ -1,36 +1,60 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
+    <button @click="setSelected('active-goals')">Active Goal</button>
+    <button @click="setSelected('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+
+  <!-- Reference code 
+  <badge-list></badge-list>
+  <user-info
+  :full-name="activeUser.name"
+  :info-text="activeUser.description"
+  :role="activeUser.role"
+></user-info>
+<course-goals>
       <template #default="slotProps">
         <h2>{{ slotProps.item }}</h2>
       </template>
-    </course-goals>
-  </div>
+    </course-goals> -->
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CoarseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CoarseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  components: {
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Liza Dolhova",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelected(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
