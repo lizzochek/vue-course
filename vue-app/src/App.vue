1fea95b9c50df182a9b114fe5b6e1ac27cd070d6
lizzochek <littled685@gmail.com>
2022-05-11 19:37:43 +0300

    Adding contacts

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <header><h1>My friends</h1></header>
+    <new-friend @add-friend="addFriend"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -18,8 +19,9 @@
 
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -46,6 +48,9 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavourite = !identifiedFriend.isFavourite;
+    },
+    addFriend(newFriend) {
+      this.friends.push(newFriend);
     },
   },
 };
@@ -82,7 +87,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -114,5 +120,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ vue-app/src/components/NewFriend.vue
@@ -0,0 +1,50 @@
+<template>
+  <form @submit.prevent="onFormSubmit">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="text" v-model="phone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="text" v-model="email" />
+    </div>
+    <button>Add a friend</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-friend"],
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: "",
+    };
+  },
+  methods: {
+    onFormSubmit() {
+      const newFriend = {
+        id: this.name.toLowerCase(),
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+      };
+
+      this.resetForm();
+      this.$emit("add-friend", newFriend);
+    },
+    resetForm() {
+      this.name = "";
+      this.phone = "";
+      this.email = "";
+    },
+  },
+};
+</script>
+
+<style></style>
