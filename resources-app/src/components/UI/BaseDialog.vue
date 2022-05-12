9309e2a5aecc59e8ab90d1799e269e85734a332b
lizzochek <littled685@gmail.com>
2022-05-12 13:13:47 +0300

    Adding teleport

--- resources-app/src/components/UI/BaseDialog.vue
+++ resources-app/src/components/UI/BaseDialog.vue
@@ -1,18 +1,20 @@
 <template>
-  <div @click="$emit('close')"></div>
-  <dialog open>
-    <header>
-      <slot name="header">
-        <h2>{{ title }}</h2>
-      </slot>
-    </header>
-    <section>
-      <slot></slot>
-    </section>
-    <menu>
-      <slot name="actions"> </slot>
-    </menu>
-  </dialog>
+  <teleport to="body">
+    <div @click="$emit('close')"></div>
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot></slot>
+      </section>
+      <menu>
+        <slot name="actions"> </slot>
+      </menu>
+    </dialog>
+  </teleport>
 </template>
 
 <script>
