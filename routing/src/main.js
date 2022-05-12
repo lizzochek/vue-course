2086b9807dc0cb18b883e98e7ced3efcc31b096d
lizzochek <littled685@gmail.com>
2022-05-12 21:54:09 +0300

    Refactoring

--- routing/src/main.js
+++ routing/src/main.js
@@ -1,52 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-
-const router = createRouter({
-  meta: {},
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      component: TeamsList,
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-      props: true,
-    },
-    {
-      path: '/users',
-      component: UsersList,
-      beforeEnter(to, from, next) {
-        console.log('users beforeEnter');
-        next();
-      },
-    },
-    { path: '/:notFound(.*)', redirect: '/teams' },
-  ],
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.afterEach(() => {
-  //sending analytics data. Logs, etc.
-  console.log('Global after each');
-});
+import router from './router';
 
 const app = createApp(App);
 
---
+++ routing/src/router.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+
+const router = createRouter({
+  meta: {},
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+      props: true,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+      beforeEnter(to, from, next) {
+        console.log('users beforeEnter');
+        next();
+      },
+    },
+    { path: '/:notFound(.*)', redirect: '/teams' },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+router.afterEach(() => {
+  //sending analytics data. Logs, etc.
+  console.log('Global after each');
+});
+
+export default router;
