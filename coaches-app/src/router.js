78aea90b6749dd81db3ec5fa1dcd1fb9c42264e3
lizzochek <littled685@gmail.com>
2022-05-14 17:15:08 +0300

    Adding pages

--- coaches-app/src/router.js
+++ coaches-app/src/router.js
@@ -1,18 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachesList from './components/pages/coaches/CoachesList.vue';
+import CoachDetail from './components/pages/coaches/CoachDetail.vue';
+import CoachesRegistration from './components/pages/coaches/CoachesRegistration';
+
+import CoachContact from './components/pages/requests/CoachContact.vue';
+import ReceivedRequests from './components/pages/requests/ReceivedRequests';
+
+import NotFound from './components/pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }],
+      component: CoachDetail,
+      children: [{ path: '/contact', component: CoachContact }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoachesRegistration },
+    { path: '/requests', component: ReceivedRequests },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
