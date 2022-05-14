5e11f85602ceede79101b4d4b178fee52d16ccc1
lizzochek <littled685@gmail.com>
2022-05-14 17:51:24 +0300

    Store for coaches

--- coaches-app/src/main.js
+++ coaches-app/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router.js';
+import store from './store/store';
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
+
 app.mount('#app');
