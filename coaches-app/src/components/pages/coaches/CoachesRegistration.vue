e3d4276ccf222c181a20ba7e568be34b45339bbb
lizzochek <littled685@gmail.com>
2022-05-14 20:12:37 +0300

    Adding registration form

--- coaches-app/src/components/pages/coaches/CoachesRegistration.vue
+++ coaches-app/src/components/pages/coaches/CoachesRegistration.vue
@@ -1,1 +1,17 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from '../../base/BaseCard.vue';
+import CoachForm from './CoachForm.vue';
+
+export default {
+  components: { CoachForm, BaseCard },
+};
+</script>
