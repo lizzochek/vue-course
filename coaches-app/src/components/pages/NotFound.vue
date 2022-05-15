26fca34f0634e0cc6b239406ded2b129e8c0f254
lizzochek <littled685@gmail.com>
2022-05-15 17:41:15 +0300

    Not found page

---
+++ coaches-app/src/components/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        Page was not found. Check our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
