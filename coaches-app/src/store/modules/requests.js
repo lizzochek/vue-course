d22b1e4b67178c4d30abddce11112a61aad5c313
lizzochek <littled685@gmail.com>
2022-05-15 13:58:11 +0300

    Storing requests

---
+++ coaches-app/src/store/modules/requests.js
@@ -0,0 +1,23 @@
+export default {
+  namespaced: true,
+  state() {
+    return {
+      requests: [],
+    };
+  },
+  mutations: {
+    addRequest(state, payload) {
+      state.requests.push(payload);
+    },
+  },
+  actions: {
+    contactCoach(context, payload) {
+      const newRequest = {
+        id: new Date().toISOString(),
+        ...payload,
+      };
+      console.log(newRequest);
+      context.commit('addRequest', newRequest);
+    },
+  },
+};
--- coaches-app/src/store/store.js
+++ coaches-app/src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches';
+import requestsModule from './modules/requests';
 
 const store = createStore({
   state() {
@@ -8,7 +9,7 @@ const store = createStore({
       userId: 'c3',
     };
   },
-  modules: { coaches: coachesModule },
+  modules: { coaches: coachesModule, requests: requestsModule },
   getters: {
     userId(state) {
       return state.userId;
